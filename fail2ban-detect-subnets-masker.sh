9b85c46bcdf77920d7764b3fab8685c8bbd97c1b
mateusz <19653795+mateusz834@users.noreply.github.com>
2020-12-05 11:18:29 +0100

    Update fail2ban-detect-subnets-masker.sh

--- fail2ban-detect-subnets-masker.sh
+++ fail2ban-detect-subnets-masker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -o nounset
 mask4=28
 mask6=64
 
@@ -14,8 +15,12 @@ masked=""
 
 sipcalc "$3" | grep ipv6 > /dev/null 2>&1
 if [ $? == '0' ]; then
+	set -o errexit
+	set -o pipefail
 	masked="$(sipcalc "$3/$mask6" | grep "Subnet prefix (masked)" | awk '{print $5}' | sed 's/\/.*//')"
 else
+	set -o errexit
+	set -o pipefail
 	masked="$(sipcalc "$3/$mask4" | grep "Network address" | awk '{print $4 }')"
 fi
 echo "$1 $masked"
