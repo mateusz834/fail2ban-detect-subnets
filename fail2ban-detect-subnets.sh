80bb76d16a946179b3a4de5340e4b48849ca7c4b
mateusz <19653795+mateusz834@users.noreply.github.com>
2020-12-05 11:14:13 +0100

    Update fail2ban-detect-subnets.sh

--- fail2ban-detect-subnets.sh
+++ fail2ban-detect-subnets.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -o nounset
+set -o errexit
 set -o pipefail
-
 journalctl -u fail2ban.service  -f -n 0 | grep -io "\[\S*] Found \S*" --line-buffered  | xargs -n3 -P 16 /usr/local/sbin/fail2ban-detect-subnets-masker.sh  | logger -t "fail2ban-detect-subnets"
