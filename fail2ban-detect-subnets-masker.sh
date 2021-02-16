8a3aef64cc9762f3fba9afa5575de6f70a84b1b9
mpoliwczak834 <mpoliwczak34@gmail.com>
2021-02-16 15:40:11 +0100

    refactor

--- fail2ban-detect-subnets-masker.sh
+++ fail2ban-detect-subnets-masker.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
 set -o nounset
-mask4=28
-mask6=64
+set -o errexit
+set -o pipefail
 
-case $1 in
-  "[ssh]") mask4=24 ;;
-  "[postfix]") ;;
-  "[dovecot]") ;;
-  "[nginx]") ;;
-  *) exit 0
-esac
+l=""
+while read line; do
+	jail="$(echo "$line" | awk '{print $1}')"
+	if [ "[$jail]" == "$1" ]; then
+		l="$line"
+		break
+	fi
+done <$config
+
+if [ -z "$l" ]; then
+	exit 0
+fi
+
+mask4="$(echo "$l" | awk '{print $2}')"
+mask6="$(echo "$l" | awk '{print $3}')"
+
+[ -z "$mask4" ] && exit 1
+[ -z "$mask6" ] && exit 1
 
 masked=""
 
-sipcalc "$3" | grep ipv6 > /dev/null 2>&1
+set +o errexit
+set +o pipefail
+sipcalc "$2" | grep ipv6 >> /dev/null 2>&1
 if [ $? == '0' ]; then
 	set -o errexit
 	set -o pipefail
-	masked="$(sipcalc "$3/$mask6" | grep "Subnet prefix (masked)" | awk '{print $5}' | sed 's/\/.*//')"
+	masked="$(sipcalc "$2/$mask6" | grep "Subnet prefix (masked)" | awk '{print $5}' | sed 's/\/.*//')"
 else
 	set -o errexit
 	set -o pipefail
-	masked="$(sipcalc "$3/$mask4" | grep "Network address" | awk '{print $4 }')"
+	masked="$(sipcalc "$2/$mask4" | grep "Network address" | awk '{print $4 }')"
 fi
 echo "$1 $masked"
--- fail2ban-detect-subnets.sh
+++ fail2ban-detect-subnets.sh
@@ -2,4 +2,41 @@
 set -o nounset
 set -o errexit
 set -o pipefail
-journalctl -u fail2ban.service  -f -n 0 | grep -io "\[\S*] Found \S*" --line-buffered  | xargs -n3 -P 16 /usr/local/sbin/fail2ban-detect-subnets-masker.sh  | logger -t "fail2ban-detect-subnets"
+
+dconfig="/usr/local/etc/fail2ban-detect-subnets/conf"
+dmasker="/usr/local/sbin/fail2ban-detect-subnets-masker.sh"
+
+function help() 
+{
+	echo "Syntax:"
+	echo -e "\t$0 [-c config] [-m masker-file]"
+	echo -e "\t\tconfig default: $dconfig"
+	echo -e "\t\tmasker-file default: $dmasker"
+	exit 1
+}
+
+config=$dconfig
+masker=$dmasker
+
+for i in "$@"; do
+	if [[ "$i" == "--help" || "$1" == "-h" ]]; then
+		help 
+	fi
+done
+
+while getopts c:m: flag
+do
+	case "${flag}" in
+	c) config="${OPTARG}";;
+	m) masker="${OPTARG}";;
+	?) help >> /dev/stderr;;
+esac
+done
+
+[ ! -x "$masker" ] && ( echo "File: $masker is not a executable or does not exist, check permissions" >> /dev/stderr; exit 1 )
+[ ! -f "$config" ] && ( echo "File: $config does not exist" >> /dev/stderr; exit 1 )
+
+
+export config
+journalctl -u fail2ban.service  -f -n 0 | grep -io "\[\S*] Found \S*" --line-buffered | awk '{print $1, $3; system("")}' | xargs -n2 -P 16 "$masker" | logger -t "fail2ban-detect-subnets"
+
